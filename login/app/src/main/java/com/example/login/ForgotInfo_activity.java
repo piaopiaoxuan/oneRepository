778f71aa69ee61c60f10f76b8654ecf90500e7c6
piaopiaoxuan <1749286261@qq.com>
2019-07-02 13:17:19 +0800

    wrote a project of login

---
+++ login/app/src/main/java/com/example/login/ForgotInfo_activity.java
@@ -0,0 +1,18 @@
+package com.example.login;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+/**
+ * Created by renkai on 17/7/6.
+ */
+
+public class ForgotInfo_activity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.forgotinfo_activity);
+    }
+}
